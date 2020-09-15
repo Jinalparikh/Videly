81cf431a7f4a7128d35f4291bc9cd1bc2d2fb42c
Jinalparikh <jinalparikh1892@gmail.com>
2020-09-15 22:20:44 +0530

    Project

---
+++ Movie_Project/Movie_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movie_Project.Startup))]
+namespace Movie_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- Movie_Rental_App/Movie_Rental_App/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Movie_Rental_App.MvcApplication" Language="C#" %>
