728151c2befbc47491e259745f6c781aebf0c812
Jinalparikh <jinalparikh1892@gmail.com>
2020-09-15 21:19:15 +0530

    default controller

---
+++ Movie_Rental_App/Movie_Rental_App/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie_Rental_App.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public string Index()
+        {
+
+            return "Hello";
+        }
+    }
+}
