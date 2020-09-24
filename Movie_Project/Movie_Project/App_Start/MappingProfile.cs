9346a54c3f3bc1ba7cf9c0b69b24475563fb5c92
Jinalparikh <jinalparikh1892@gmail.com>
2020-09-24 15:52:49 +0530

    Building of web Api

---
+++ Movie_Project/Movie_Project/App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Movie_Project.Dtos;
+using Movie_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_Project.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>();
+        }
+    }
+}
